341f37af508a79bd885365ddeb674202ae9fb469
Andie Kardos <58579363+AndrejaKardos@users.noreply.github.com>
2024-12-10 17:08:43 +1100

    Adjust Header styling to ensure it's above other content

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -2,13 +2,13 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-indigo-800 bg-opacity-80 text-white p-4 sm:p-6 lg:p-8 fixed w-full top-0 left-0 z-10 shadow-md">
+    <header className="h-[6vh] bg-indigo-800 bg-opacity-80 text-white p-2 sm:p-4 lg:p-6 shadow-md">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-3xl font-semibold">Unreal Deals</div>
+        <div className="text-2xl sm:text-3xl font-semibold">Unreal Deals</div>
         <nav>
-          <ul className="flex justify-center space-x-12">
+          <ul className="flex justify-center items-center space-x-8">
             <li>
-              <Link href="#products" className="hover:text-yellow-300 text-lg">
+              <Link href="/products" className="hover:text-yellow-300 text-lg">
                 Products
               </Link>
             </li>
